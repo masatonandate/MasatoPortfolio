d3ebfcc0393d87b3e10f041d64b3d9971e9238fe
masatonandate <masatonandate@gmail.com>
2024-04-25 12:10:21 -0700

    Added Resume

--- src/Links/Resume.js
+++ src/Links/Resume.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Header from '../Component/Header';
 import Footer from '../Component/Footer';
+import PDF from '../PDF/Nandate_Masato.Resume.pdf'
 
 
 const Resume = () => {
   return (
     <div>
       <Header />
+      <div className='flex items-center bg-white dark:bg-gray-900'>
+        <iframe
+          src = {PDF}
+          className=" w-3/4 mx-auto aspect-square border-none inline-block"
+          title="MasatoResume">
+        </iframe>
+      </div>
       <Footer />
     </div>
   )
